58c2d050081dc51d31b5843c09a621e919c931e9
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-06-02 16:47:18 -0300

    adicionando um tratamento para a data

---
+++ prisma/migrations/20230602174550_atualizando_tabela/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `provas` MODIFY `dataProgramada` DATETIME(0) NOT NULL;
--- src/controllers/ProvasController.ts
+++ src/controllers/ProvasController.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from 'express'; 
 import {PrismaClient} from '@prisma/client';
+import { parseISO } from 'date-fns';
 
 class ProvasController {    
     
@@ -43,40 +44,71 @@ class ProvasController {
         res.status(200).json(prova);    
     }
 
-    async store(req:Request,res:Response)
-    {        
-        const prisma = new PrismaClient();              
-        const dados = req.body;        
-        const novaProva = await prisma.prova.create(
-            {                
-                data: dados,                
-                select: {                    
-                    id:true,                    
-                    dataProgramada:true,                    
-                    tipo:true,
-                    peso:true             
-                }            
-            }        
-        );        
-        res.status(200).json(novaProva);    
+
+    async store(req: Request, res: Response) {
+      const prisma = new PrismaClient();
+      const dados = req.body;
+    
+      // Extrair as propriedades relevantes do objeto `dados`
+      const { dataProgramada, tipo, peso, turmaId } = dados;
+    
+      // Criar a nova prova
+      const novaProva = await prisma.prova.create({
+        data: {
+          dataProgramada: parseISO(dataProgramada), // Converter a string para o formato desejado
+          tipo,
+          peso,
+          turma: {
+            connect: { id: turmaId } // Conectar a prova à turma existente pelo ID
+          }
+        },
+        select: {
+          id: true,
+          dataProgramada: true,
+          tipo: true,
+          peso: true
+        }
+      });
+    
+      res.status(200).json(novaProva);
     }
+    
 
     async update(req:Request,res:Response)
-    {        
+    {
         const prisma = new PrismaClient();        
-        const provaAlterada = await prisma.prova.update(            
-            {                
-                where: {id: Number(req.params.id) },                
-                data: req.body,                
-                select: {                    
-                    id:true,                    
-                    dataProgramada:true,                    
-                    tipo:true,
-                    peso:true                
-                }            
-            }        
-        );        
-        res.status(200).json(provaAlterada);    
+        const provaId = Number(req.params.id);
+        const dados = req.body;  
+        const { dataProgramada, tipo, peso } = dados;
+        const provaAlterada = await prisma.prova.update({
+            where: { id: provaId },
+            data: {
+              dataProgramada: parseISO(dataProgramada), // Converter a string para o formato desejado
+              tipo,
+              peso
+            },
+            select: {
+              id: true,
+              dataProgramada: true,
+              tipo: true,
+              peso: true
+            }
+          });
+          res.status(200).json(provaAlterada);
+      
+        // const provaAlterada = await prisma.prova.update(            
+        //     {                
+        //         where: {id: Number(req.params.id) },                
+        //         data: req.body,                
+        //         select: {                    
+        //             id:true,                    
+        //             dataProgramada:true,                    
+        //             tipo:true,
+        //             peso:true                
+        //         }            
+        //     }        
+        // );        
+        // res.status(200).json(provaAlterada);    
     }
 
     async delete(req:Request,res:Response)
